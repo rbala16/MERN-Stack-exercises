fa870d58e4d727dc0cce7ed7ccc8e42ba6aa7a7f
Rajni Bala <balatamna16@gmail.com>
2024-03-17 16:09:30 -0400

    delete the data

--- client/src/context/WorkoutContext.js
+++ client/src/context/WorkoutContext.js
@@ -13,6 +13,10 @@ export const workoutsReducer = (state, action) => {
       return { 
         workouts: [action.payload, ...state.workouts] 
       }
+      case 'DELETE_WORKOUT':
+        return{
+          workouts:state.workouts.filter((w)=> w._id !==action.payload)
+        }
     default:
       return state
   }
