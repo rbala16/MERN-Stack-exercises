d3c5bd5cd83c4c5cf65163b78f7b04ec2295fda8
Rajni Bala <balatamna16@gmail.com>
2024-03-17 16:15:50 -0400

    file updated

--- client/src/context/WorkoutContext.js
+++ client/src/context/WorkoutContext.js
@@ -15,7 +15,7 @@ export const workoutsReducer = (state, action) => {
       }
       case 'DELETE_WORKOUT':
         return{
-          workouts:state.workouts.filter((w)=> w._id !==action.payload)
+          workouts:state.workouts.filter((w)=> w._id !==action.payload._id)
         }
     default:
       return state
